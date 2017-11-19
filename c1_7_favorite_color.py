ff3552f8745d1c6e2e2829bbc42e369a98f26127
bmutarelli <bmutarelli@ideaventionsacademy.org>
2017-11-19 17:40:54 -0500

    Favorite_Color V1

--- c1_7_favorite_color.py
+++ c1_7_favorite_color.py
@@ -1,5 +1,28 @@
-# Name
-# Date
+# Brogan
+# November 19 2017
 # Python Beginnings
 # 7 Favorite Color
+
+yes = "yes"
+
+color = "blue"
+
+user_color = input ("What is your favorite color?  ")
+
+while user_color == color:
+
+    print ("Me too! What a coincidence!")
+    yes = "no"
+    break
+
+if yes == "yes":
+
+    print ("Thats not my favorite color. What is wrong with you. You should have the same favorite color as me!")
+
+print ("""
+
+See ya later!""")
+
+
+
 
