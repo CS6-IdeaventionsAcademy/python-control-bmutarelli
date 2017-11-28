4fcb917bda31ec39b94787a1078fd3f0e1039b59
neberhart <32400919+neberhart@users.noreply.github.com>
2017-11-28 12:07:28 -0500

    Door V2

--- BehindTheDoor.py
+++ BehindTheDoor.py
@@ -14,20 +14,21 @@ door = random.randint (1,4)
 print ("You are on the last question")
 user_door = input ("Pick your door (1-4): ")
 
-if user_door not in ["1", "2", "3", "4"]:
-    print ("That is invalid")
-    user_door = input ("Pick your door (1-4): ")
 
-if user_door == door1:
+
+if user_door == "1":
     print ("You got a brand new " + door1)
     
-elif user_door == door2:
+elif user_door == "2":
     print ("You got a brand new " + door4)
 
-elif user_door == door3:
+elif user_door == "3":
     print ("You got a brand new " + door3)
 
-elif user_door == door4:
+elif user_door == "4":
     print ("You got a brand new " + door4)
 
 
+else user_door not in ["1", "2", "3", "4"]:
+    print ("That is invalid")
+    user_door = input ("Pick your door (1-4): ")
