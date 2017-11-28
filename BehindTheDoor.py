f668f9be90f2cd4a79b0139e1dd2a62fcb3b9436
neberhart <32400919+neberhart@users.noreply.github.com>
2017-11-28 12:05:09 -0500

    Door V1

---
+++ BehindTheDoor.py
@@ -0,0 +1,33 @@
+#Brogan Mutarelli
+#Behind the door game show
+#CS
+
+door1 = "Iphone X"
+door2 = "Egostia"
+door3 = "Freshly Used Toilet (Unflushed)"
+door4 = "$1000"
+
+import random
+
+door = random.randint (1,4)
+
+print ("You are on the last question")
+user_door = input ("Pick your door (1-4): ")
+
+if user_door not in ["1", "2", "3", "4"]:
+    print ("That is invalid")
+    user_door = input ("Pick your door (1-4): ")
+
+if user_door == door1:
+    print ("You got a brand new " + door1)
+    
+elif user_door == door2:
+    print ("You got a brand new " + door4)
+
+elif user_door == door3:
+    print ("You got a brand new " + door3)
+
+elif user_door == door4:
+    print ("You got a brand new " + door4)
+
+
