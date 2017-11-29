b033f129e549241a7c2370176556f660ee9c3727
neberhart <32400919+neberhart@users.noreply.github.com>
2017-11-29 11:53:15 -0500

    dOOR v4

--- BehindTheDoor.py
+++ BehindTheDoor.py
@@ -3,13 +3,16 @@
 #CS
 
 import random
-
+import time
 door1 = "Iphone X"
 door2 = "Egostia"
 door3 = "Freshly Used Toilet (Unflushed)"
 door4 = "$1000"
 
+def cls():
+    print ('\n' * 60)
 
+cls()
 
 door = random.randint (1,4)
 
@@ -20,6 +23,7 @@ again = ["yes", "Yes", "Y", "y", "yep", "Yep"]
 answers = ["1", "2", "3", "4"]
 
 print ("You are on the last question!!!!")
+time.sleep(1)
 print ("You have made it to the last stage!!!!")
 
 while play_again in again:
@@ -33,15 +37,16 @@ while play_again in again:
 
     if user_door == "1":
         print ("You got a brand new " + door1)
-        
+        time.sleep (1)
     elif user_door == "2":
         print ("You got a brand new " + door2)
-
+        time.sleep (1)
     elif user_door == "3":
         print ("You got a brand new " + door3)
-
+        time.sleep (1)
     elif user_door == "4":
         print ("You got a brand new " + door4)
+        time.sleep (1)
 
     play_again = input ("Do you want to play again (Y/N)?  ")
 
